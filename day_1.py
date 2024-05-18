1c08be1c3a24c84b0c8dc4759b78118fcc7add89
Kushal Kamle <kushalkamle64@gmail.com>
2024-05-18 17:15:28 +0530

    Band name generator proj done.

--- day_1.py
+++ day_1.py
@@ -1,5 +1,12 @@
 # person = ("Hello " + str(len(input("What is your name? "))) + "!")
 # print(person)
 
-person = ("Hey! " + "Time is "+ str(input("Whats the time? ")))
-print(person)
+# person = ("Hey! " + "Time is "+ str(input("Whats the time? ")))
+# print(person)
+
+# Band name generator
+city_name = input("In which city you were born? ")
+print("OK! You were born in " + city_name)
+pet_name = input("whats the name of your pet name? ")
+print("Your pet name is " + pet_name)
+print("Your Band name could be " + city_name + " " + pet_name)
