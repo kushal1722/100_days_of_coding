aa42e24ea2fa417fab4b4b94373335818306aed6
Kushal Kamle <kushalkamle64@gmail.com>
2024-05-19 20:07:40 +0530

    example added.

--- day_2.py
+++ day_2.py
@@ -7,7 +7,14 @@
 # print(bmi_as_int)
 
 #f-strings
-score = 0
-height = 1.8
-isWinning = True
-print(f"your score is {score}, your height is {height}, your winning is {isWinning}")
+# score = 0
+# height = 1.8
+# isWinning = True
+# print(f"your score is {score}, your height is {height}, your winning is {isWinning}")
+
+# example
+age = input("Please enter your age: ")
+print(f"Your age is {age}")
+years = 90 - int(age)
+weeks = years * 52
+print(f"You have {weeks} weeks left.")
