5b568ae76333fb3faad5675686fb03e37fe27e3b
Kushal Kamle <kushalkamle64@gmail.com>
2024-05-19 20:41:21 +0530

    Tip calculator proj added.

--- day_2.py
+++ day_2.py
@@ -13,8 +13,20 @@
 # print(f"your score is {score}, your height is {height}, your winning is {isWinning}")
 
 # example
-age = input("Please enter your age: ")
-print(f"Your age is {age}")
-years = 90 - int(age)
-weeks = years * 52
-print(f"You have {weeks} weeks left.")
+# age = input("Please enter your age: ")
+# print(f"Your age is {age}")
+# years = 90 - int(age)
+# weeks = years * 52
+# print(f"You have {weeks} weeks left.")
+
+# Tip calculator
+print("Welcome to tip calculator project!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much % would you like to tip 10, 12 or 15? "))
+people = int(input("How many people to split the bill? "))
+tip_as_percent = tip / 100
+total_tip_amount = tip_as_percent * bill
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_bill = round(bill_per_person, 2)
+print(f"Each person should pay: ${final_bill}")
