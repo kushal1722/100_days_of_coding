9c5870d9f3b4f26bf8cfef80801a73d44f65492b
Kushal Kamle <kushalkamle64@gmail.com>
2024-05-19 19:58:46 +0530

    F-string example.

--- day_2.py
+++ day_2.py
@@ -1,7 +1,13 @@
-height = input("What is your height? ")
-weight = input("What is your weight? ")
-weight_as_int = int(weight)
-height_as_float = float(height)
-bmi = weight_as_int / height_as_float ** 2
-bmi_as_int = int(bmi)
-print(bmi_as_int)
+# height = input("What is your height? ")
+# weight = input("What is your weight? ")
+# weight_as_int = int(weight)
+# height_as_float = float(height)
+# bmi = weight_as_int / height_as_float ** 2
+# bmi_as_int = int(bmi)
+# print(bmi_as_int)
+
+#f-strings
+score = 0
+height = 1.8
+isWinning = True
+print(f"your score is {score}, your height is {height}, your winning is {isWinning}")
