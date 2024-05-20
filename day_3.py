0c9b0c25f47f1893d600d58de778ce60953f14f2
Kushal Kamle <kushalkamle64@gmail.com>
2024-05-20 11:43:54 +0530

    added few lines for the roller coaster example.

--- day_3.py
+++ day_3.py
@@ -1,13 +1,26 @@
 print("Welcome to the Water Park!")
 height = int(input("What is your height? "))
+bill = 0
+
 if height >= 120:
     print("You can ride the roller coaster.")
+
     age = int(input("What is your age? "))
     if age < 12:
+        bill = 5
         print("You have to pay $5 for the ticket.")
     elif age <= 18:
+        bill = 7
         print("You have to pay $7 for the ticket.")
     else:
+        bill = 12
         print("You have to pay $12 for the ticket.")
+
+    want_photo = input("Do you like to take a photo? Y or N: ")
+    if want_photo == "Y":
+        bill += 3
+
+    print(f"Your total bill is ${bill}")
+
 else:
     print("You have to grow taller to ride roller coaster.")
